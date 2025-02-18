dfd532c7e39f923e13f5b5e5071d60c62c01ed4b
Kriharini <krithika.ha@gmail.com>
2025-02-18 18:30:20 -0500

    testusingjest

---
+++ number.js
@@ -0,0 +1,5 @@
+function getEvenNumbers(arr) {
+    return arr.filter(num => num % 2 === 0);
+  }
+  
+  module.exports = {getEvenNumbers};
---
+++ number.test.js
@@ -0,0 +1,17 @@
+const {getEvenNumbers} = require('./number.js');
+
+test('returns even numbers from an array', () => {
+  expect(getEvenNumbers([1, 2, 3, 4, 5, 6])).toEqual([2, 4, 6]);
+});
+
+test('returns an empty array if no even numbers', () => {
+  expect(getEvenNumbers([1, 3, 5, 7])).toEqual([]);
+});
+
+test('returns the same array if all numbers are even', () => {
+  expect(getEvenNumbers([2, 4, 6, 8])).toEqual([2, 4, 6, 8]);
+});
+
+test('handles an empty array', () => {
+  expect(getEvenNumbers([])).toEqual([]);
+});
